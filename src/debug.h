2425fc8b2a4ac519660f4bd16efe370089991f38
enderagent <97055625+enderagent0@users.noreply.github.com>
2022-05-02 15:52:34 +0100

    Delete src directory

--- src/debug.h
+++
@@ -1,6 +0,0 @@
-
-#pragma once
-#include <stdio.h>
-#define PRINTLINE(x) printf("%s\n", x)
-#define PRINTLINE_ERROR(x) printf("[-] %s\n", x)
-
--- src/string.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-#include <stdio.h>
-
-char* AllocateString(size_t length);
-char* FillString(const char* value);
-
